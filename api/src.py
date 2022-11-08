66ff9de3cf2503acc6eace4241436283f1e37611
TatiDaniel <tatianamartinez45@gmail.com>
2022-11-08 20:38:47 -0300

    entre

--- api/src.py
+++ api/src.py
@@ -9,12 +9,12 @@ openai.api_key =os.environ.get("OPENAI_KEY")
 
 def userMessage(payload, conversationId):
 
-    contexto = "\n Datos útiles para el asistente virtual llamado AgileBot :contenido y recursos significa teoría a demanda significa que puede tomar la actividad en cualquier momento si es ademanda las actividades se deben realizar según el orden cuando el estudiante pregunta cual es mi próxima clase, responder con todo el detalle de la próxima actividad tipo clase que aún no inicia, en base al día de hoy siempre enviar el link de la actividad"
-    contexto += "\n Datos de la Empresa AGILEBOT: Soporte Técnico: mesadeayuda@agilebot.cl"
-    contexto += "\n El asistente responde en español a estudiantes Argentinos, agrega siempre emoticones a sus respuestas tales como 😁😉🙂☺️🤖, es útil, creativo, inteligente, muy amable y simpático. Datos del Estudiante al que responde AGILEBOT:estudiantes inscritos en al Institución UTN. cursos que toma el alumno en UTN Curso: Habilidades Blandas" 
-    #contexto += "\n UML es una herramienta en el mundo actual del desarrollo de sistemas. Permite a los creadores de sistemas generar diseños que capturen sus ideas en una forma convencional y fácil de comprender para comunicarlas a otras personas. Está compuesto por diversos elementos gráficos que se combinan para conformar diagramas. La finalidad de los diagramas es presentar diversas perspectivas de un sistema a las cuales se les conoce como modelo."
-    #contexto += "\n Diagrama de clases, Una clase es una categoría o grupo de cosas que tienen atributos y acciones similares."
-    #contexto += "\n Un objeto es una instancia de clase, una entidad que tiene valores especificos de los atributos y acciones"
+    #contexto = "\n Datos útiles para el asistente virtual llamado AgileBot :contenido y recursos significa teoría a demanda significa que puede tomar la actividad en cualquier momento si es ademanda las actividades se deben realizar según el orden cuando el estudiante pregunta cual es mi próxima clase, responder con todo el detalle de la próxima actividad tipo clase que aún no inicia, en base al día de hoy siempre enviar el link de la actividad"
+    #contexto += "\n Datos de la Empresa AGILEBOT: Soporte Técnico: mesadeayuda@agilebot.cl"
+    #contexto += "\n El asistente responde en español a estudiantes Argentinos, agrega siempre emoticones a sus respuestas tales como 😁😉🙂☺️🤖, es útil, creativo, inteligente, muy amable y simpático. Datos del Estudiante al que responde AGILEBOT:estudiantes inscritos en al Institución UTN. cursos que toma el alumno en UTN Curso: Habilidades Blandas" 
+    contexto += "\n UML es una herramienta en el mundo actual del desarrollo de sistemas. Permite a los creadores de sistemas generar diseños que capturen sus ideas en una forma convencional y fácil de comprender para comunicarlas a otras personas. Está compuesto por diversos elementos gráficos que se combinan para conformar diagramas. La finalidad de los diagramas es presentar diversas perspectivas de un sistema a las cuales se les conoce como modelo."
+    contexto += "\n Diagrama de clases, Una clase es una categoría o grupo de cosas que tienen atributos y acciones similares."
+    contexto += "\n Un objeto es una instancia de clase, una entidad que tiene valores especificos de los atributos y acciones"
 
     # obtner la lista de registros guardados en la base de datos por converstaionId
     
